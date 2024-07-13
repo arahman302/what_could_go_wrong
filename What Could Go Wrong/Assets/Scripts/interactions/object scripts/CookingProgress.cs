2f63ff175ef4636cc8a8ebeec6d72b0c731a9394
Arshiya Rahman <arshiyar2005@gmail.com>
2024-07-13 18:40:49 -0400

    timer fix

--- What Could Go Wrong/Assets/Scripts/interactions/object scripts/CookingProgress.cs
+++ What Could Go Wrong/Assets/Scripts/interactions/object scripts/CookingProgress.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 public class CookingProgress : MonoBehaviour {
-    private float levelTimeLeft = 90f;
+    [SerializeField] private float timePassed = 0;
+    [SerializeField] private float timeRemaining = 90f;
     private bool timeIsRunning = true;
     [SerializeField] private TMP_Text timerText;
     
     void Start() {
         timeIsRunning = true;
+        Time.timeScale = 1.0f;
     }
 
     void Update() {
-        while (timeIsRunning) {
-            if (levelTimeLeft <= 0) {
-                timeIsRunning = false;
+        //inside while loop, code seems to end immedietly
+        if (timeRemaining >= 0) {
+            //outside of while loop, code runs at expected pace
+            //timePassed += Time.deltaTime;
+            timeRemaining -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(timeRemaining / 60);
+            int seconds = Mathf.FloorToInt(timeRemaining % 60);
+            if (seconds >= 0) {
+                timerText.text = string.Format("{0:00}: {1:00}", minutes, seconds);
             }
-            levelTimeLeft += Time.deltaTime;
-            DisplayProgress(levelTimeLeft);
+            
         }
-    }
-
-    void DisplayProgress(float timeToDisplay) {
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        // if (timePassed > 90) {
+        //     timeIsRunning = false;
+        // }
     }
 }
---
+++ What Could Go Wrong/Assets/Scripts/interactions/object scripts/DeeppanCook.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeeppanCook : MonoBehaviour
+{
+    [SerializeField] List<GameObject> acceptableItems;
+    public void cook() {
+        return;
+    }
+    public int finishedCooking() {
+        return 1;
+    }
+}
